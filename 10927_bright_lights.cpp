71103c2483541fa76a59085715146b3b06d9897e
Ygor Hora <ygor.hora@visagio.com>
2017-07-11 23:50:37 -0300

    Bright light problem corrected

--- 10927_bright_lights.cpp
+++ 10927_bright_lights.cpp
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <utility>
-#include <cmath>
+#include <math.h>
 #include <limits>
 #include <vector>
 #include <utility>
 #include <algorithm>
 
+#define PI 3.14159265
+
 using namespace std;
 
 typedef pair<int,int> pii;
+typedef pair<double,double> pdd;
 typedef unsigned long long ull;
+typedef long double lld;
 
-vector< pair<double, pair<ull, pair< pii, int> > > > vect;
+vector< pair<double, pair<ull, pair< pdd, int> > > > vect;
 // {angle -> {distance -> { {x -> y} -> height} }
 
 bool areSame(double a, double b) {
     return fabs(a - b) < numeric_limits<double>::epsilon();
 }
 
-int calculateLastIdx(vector< pair<double, pair<ull, pair< pair<int,int>, int> > > > &v, int firstIdx){
+int calculateLastIdx(vector< pair<double, pair<ull, pair< pdd, int> > > > &v, int firstIdx){
 	int lastIdx = firstIdx;
 	while(areSame(v[lastIdx].first, v[firstIdx].first)){
 		lastIdx++;
@@ -30,52 +34,43 @@ int calculateLastIdx(vector< pair<double, pair<ull, pair< pair<int,int>, int> > 
 	return lastIdx;
 }
 
-double calculateAngle(ull x, ull y){
-	ull x0 = 1, y0 = 0;
-	if(x*y0-x0*y > 0) {
-		
-	} else if (x*y0-x0*y < 0){
-		
-	} else {
-		if()
-	}
-}
-
 int main(){
 	int n;
 	int dataset = 1;
 	while(scanf("%d", &n), n != 0){
 		for(int i = 0; i < n; ++i){
-			ull x, y;
+			double x, y;
 			int height;
-			scanf("%llu %llu %d", &x, &y, &height);
-			pii coord = make_pair(x, y);
-			pair<pii, int> elem = make_pair(coord, height);
+			scanf("%lf %lf %d", &x, &y, &height);
+			pdd coord = make_pair(x, y);
+			pair<pdd, int> elem = make_pair(coord, height);
 			
 			ull distance = x*x + y*y;
-			//printf("Distance = %llu\n\n", distance);
-			pair<int, pair<pii, int> > data1 = make_pair(distance, elem);
 			
-			double angle = calculateAngle(x, y, sqrt(distance));
-			pair<double, pair<int, pair<pii, int> > > data2 = make_pair(angle, data1);
+			pair<ull, pair<pii, int> > data1 = make_pair(distance, elem);
+			
+			//double angle = calculateAngle(x, y, sqrt(distance));
+			double angle = atan2 (y, x) * 180 / PI;
+			pair<double, pair<ull, pair<pii, int> > > data2 = make_pair(angle, data1);
 			
 			vect.push_back(data2);
 		}
 		
 		sort(vect.begin(), vect.end());
 		
-		
+		/*
 		for(int i = 0; (unsigned) i < vect.size(); i++){
-			printf("x = %d, y = %d\n", vect[i].second.second.first.first,vect[i].second.second.first.second);
+			printf("x = %lf, y = %lf\n", vect[i].second.second.first.first,vect[i].second.second.first.second);
 			printf("Angle = %lf\n", vect[i].first);
-			printf("Altura = %d\n\n", vect[i].second.second.second);
-		}
+			printf("Altura = %d\n", vect[i].second.second.second);
+			printf("Distance = %llu\n\n", vect[i].second.first);
+		}*/
 		
 		
 		vector< pii > ans;
 		for(int firstIdx = 0; (unsigned) firstIdx < vect.size();){
 			int lastIdx = calculateLastIdx(vect, firstIdx);
-			printf("First Idx: %d,Last Idx: %d\n", firstIdx, lastIdx);
+			//printf("First Idx: %d,Last Idx: %d\n", firstIdx, lastIdx);
 			
 			// first to last indexes calculated have same angle now [)
 			int hHeight = vect[firstIdx].second.second.second;
@@ -84,7 +79,7 @@ int main(){
 				if(height > hHeight){
 					hHeight = height;
 				} else {
-					printf("%d %d\n", firstIdx, i);
+					//printf("%d %d\n", firstIdx, i);
 					ans.push_back(vect[i].second.second.first);
 				}
 			}
